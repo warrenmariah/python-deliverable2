2a20932a6ec1ef8e2c3ec343780d99f3c447613f
Mariah Warren <msmith12346@gmail.com>
2024-05-31 03:52:04 -0400

    resubmitting deliverable

--- main.py
+++ main.py
@@ -3,25 +3,24 @@ fruit_options = ["Apple", "Grape", "Orange"]
 apple_cost = 2
 grape_cost = 1
 orange_cost = 3
-print(f"1. {fruit_options[0]} $2")
-print(f"2. {fruit_options[1]} $1")
-print(f"3. {fruit_options[2]} $3")
-apple_selection = int(input(f"Welcome{customer_name}. How many apples would you like to buy?"))
-print(f"You bought {apple_selection} apple(s).")
-grape_selection = int(input(f"Welcome{customer_name}. How many grapes would you like to buy?"))
-print(f"You bought {grape_selection} grape(s).")
-orange_selection = int(input(f"Welcome{customer_name}. How many oranges would you like to buy?"))
-print(f"You bought {orange_selection} orange(s).")
-number_apple = apple_selection
-number_grape = grape_selection
-number_orange = orange_selection
+fruit_selection = int(input(f"Welcome{customer_name}. Which fruit would you like to buy?" f"\n1. {fruit_options[0]} $2" f"\n2. {fruit_options[1]} $1" f"\n3. {fruit_options[2]} $3"))
+print(f"You bought {fruit_selection}")
+number_apple = fruit_selection
+number_grape = fruit_selection
+number_orange = fruit_selection
 subtotal = int(apple_cost * number_apple + grape_cost * number_grape + orange_cost * number_orange)
 tax = 0.05 * subtotal
 total = subtotal + tax
-print(f"Receipt for{customer_name}")
-print(f"{number_apple} apple(s) at $2 apiece")
-print(f"{number_grape} grape(s) at $1 apiece")
-print(f"{number_orange} orange(s) at $3 apiece")
-print(f"Subtotal: ${subtotal}")
-print(f"5% Tax: ${tax}")
-print(f"Total: ${total}")
+
+while True:
+    response = input("Would you like to buy another piece of fruit? yes or no")
+    if response == "yes":
+        print(fruit_selection)
+    else:
+        print(f"Receipt for{customer_name}")
+        print(f"{number_apple} apple(s) at $2 apiece")
+        print(f"{number_grape} grape(s) at $1 apiece")
+        print(f"{number_orange} orange(s) at $3 apiece")
+        print(f"Subtotal: ${subtotal}")
+        print(f"5% Tax: ${tax}")
+        print(f"Total: ${total}")
